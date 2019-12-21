38cba60e03429504a45908346e41acb8c40891a8
OlgaPaw <olga.pawlowicz@gmail.com>
2019-12-21 23:42:28 +0100

    Heroku deloyment

--- api/database.py
+++ api/database.py
@@ -1,10 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./larp_callendar.db'
+POSTRGES_LOCAL = 'postgres://postgres:mysecretpassword@postgres:5432'
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL', POSTRGES_LOCAL)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+postgres_args = {'sslmode': 'allow'} if os.environ.get('DATABASE_URL') else {}
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=postgres_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
